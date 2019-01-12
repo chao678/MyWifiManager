19586c556c4ddaae7a65a24082c0e05af9448316
chao678 <674313699@qq.com>
2019-01-12 17:21:48 +0800

    first commit

---
+++ app/src/main/java/com/example/mywifimanager/MyWifiActivity.java
@@ -0,0 +1,13 @@
+package com.example.mywifimanager;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyWifiActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mywifi);
+    }
+}
